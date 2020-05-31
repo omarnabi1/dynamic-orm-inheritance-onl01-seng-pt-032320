17035699684d83271eafbae5ba7854f27e5984a4
Omar Nabi <onabi20@gmail.com>
2020-05-31 17:18:25 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -32,9 +32,7 @@ class InteractiveRecord
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
-  def table_name_for_insert
-    self.class.table_name
-  end
+  t
 
   def values_for_insert
     values = []
